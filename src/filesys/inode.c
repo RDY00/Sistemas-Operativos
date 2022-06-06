96c2d2f5eec28d59d5c73b950ad001a93d953bdf
RDY00 <thescrocherstakss02@gmail.com>
2022-06-06 00:38:39 -0500

    Inicio practica extra

--- src/filesys/inode.c
+++ src/filesys/inode.c
@@ -14,7 +14,8 @@
    Must be exactly BLOCK_SECTOR_SIZE bytes long. */
 struct inode_disk
   {
-    block_sector_t start;               /* First data sector. */
+    // block_sector_t start;               /* First data sector. */
+    struct list file_blocks;            /* List of file blocks */
     off_t length;                       /* File size in bytes. */
     unsigned magic;                     /* Magic number. */
     uint32_t unused[125];               /* Not used. */
